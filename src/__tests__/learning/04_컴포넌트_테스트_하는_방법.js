2445e3525fc9f5e15bf78c5105653de9ac11f4a3
Seongbin Kim <seongbin9786@gmail.com>
2018-12-02 09:34:45 +0900

    enzyme 테스트 작성중...

---
+++ src/__tests__/learning/04_컴포넌트_테스트_하는_방법.js
@@ -0,0 +1,11 @@
+import { shallow } from 'enzyme';
+import TestComp from '../../learning/components/TestComp';
+
+describe('TestComp 테스트', () => {
+    test('TestComp를 렌더링하면 div여야 한다.', () => {
+        expect(1 + 1).toBe(2);
+    });
+    test('테스트 케이스 정의 2', () => {
+
+    });
+});
