5cfda2c4f1a45bf925d2b5bae529f9f9e2c1f099
seongbin9786 <seongbin9786@gmail.com>
2018-12-09 18:24:41 +0900

    폴더 구조 예시 추가

---
+++ src/containers/BasicContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import BasicComponent from '../components/BasicComponent';
+
+const BasicContainer = () => (
+    <div>
+        <div>기본 컨테이너입니다.</div>
+        <BasicComponent />
+    </div>
+);
+
+export default BasicContainer;
--- src/pages/BasicPage.js
+++ src/pages/BasicPage.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import BasicContainer from '../containers/BasicContainer';
 
-const BasicPage = () => <div>기본 페이지입니다.</div>;
+const BasicPage = () => (
+    <div>
+        <div>기본 페이지입니다.</div>
+        <BasicContainer />
+    </div>
+);
 
 export default BasicPage;
