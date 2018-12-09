3e3577132bef2baa34e723ba395b2b31274a8a31
seongbin9786 <seongbin9786@gmail.com>
2018-12-09 13:52:27 +0900

    BasicPage 추가

--- src/Routes.js
+++ src/Routes.js
@@ -1,3 +1,11 @@
 import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import BasicPage from './pages/BasicPage';
 
-export default () => <div />;
+export default () => (
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" component={BasicPage} />
+        </Switch>
+    </BrowserRouter>
+);
---
+++ src/pages/BasicPage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const BasicPage = () => <div>기본 페이지입니다.</div>;
+
+export default BasicPage;
