c814e41cc17907d9e5e3d06c6e68aee5204a4518
easywaldo <acetious@gmail.com>
2018-12-12 00:21:42 +0900

    메서드 이름을 변경한다
    그외에 UI 를 AppBar 와 Toolbar 로 수정함.
    
    Issue : #14

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,61 +1,43 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
-import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Popover from '@material-ui/core/Popover';
-import red from '@material-ui/core/colors/red';
-
-const styles = theme => ({
-  root: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'flex-end',
-  },
-  icon: {
-    margin: theme.spacing.unit * 2,
-  },
-  iconHover: {
-    margin: theme.spacing.unit * 2,
-    '&:hover': {
-      color: red[800],
-    },
-  },
-});
-
 
 class Counter extends Component {
     state = {
-        count: 0
+        id: this.props.id,
+        productName: this.props.name,
+        count: 1,
+        error: false,
     }
 
-    addCount = () => {
+    increamentCount = () => {
         this.setState({
             count: this.state.count + 1
         })
     }
 
-    substract = () => {
+    decreamentCount = () => {
         if (this.state.count <= 0) return;
         this.setState({
             count: this.state.count - 1
         })
     }
 
-    render() {
-        const { classes } = this.props;
 
+    render() {
         return (
-            <div className={classes}>
-                <Button variant="contained" color="primary" onClick={this.substract}>-</Button>
-                <Paper>
-                    <Typography variant="h5" component="h3">
-                    Counter
-                    </Typography>
-                    <Typography component="p">
-                    {this.state.count}
-                    </Typography>
-                </Paper>
-                <Button variant="contained" color="primary" onClick={this.addCount}>+</Button>
+            <div>
+                    <AppBar position="static" color="default">
+                        <Typography variant="h6" color="inherit">
+                            <Toolbar>
+                            <Button variant="contained" color="primary" onClick={this.decreamentCount}>-</Button>
+                                상품 : {this.props.product.name} 수량 : {this.state.count}
+                            <Button variant="contained" color="primary" onClick={this.increamentCount}>+</Button>
+                            </Toolbar>
+                        </Typography>
+                    </AppBar>
             </div>
         );
     }
