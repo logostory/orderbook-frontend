de837865d4e140b9e956fa13eb5e22f4fc667143
torypapa <torypapa.io@gmail.com>
2018-12-13 23:08:17 +0900

    update to hashrouter

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Switch, Route, BrowserRouter, HashRouter } from 'react-router-dom';
 import BasicPage from './pages/BasicPage';
 import CartPage from './pages/CartPage';
 import Payment from './pages/Payment';
@@ -7,12 +7,13 @@ import Loading from './pages/Loading';
 
 
 export default () => (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+
+    <HashRouter>
         <Switch>
             <Route exact path="/" component={Loading} />
             <Route path="/basic" component={BasicPage} />
             <Route path="/cart" component={CartPage} />
             <Route path="/payment" component={Payment} />
         </Switch>
-    </BrowserRouter>
+    </HashRouter>
 );
