cc8bc01a862f5edb017f1c8ac17020a8d5916005
exntu.metatron <wonjune@bliex.com>
2018-12-21 17:43:15 +0900

    #28 create order confirm page

---
+++ src/pages/OrderConfirmPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class OrderConfirmPage extends Component {
+    render() {
+
+        return (
+            <React.Fragment>
+
+            </React.Fragment>
+        );
+    }
+}
+export default OrderConfirmPage;
