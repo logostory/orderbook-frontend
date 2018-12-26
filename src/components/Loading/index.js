3122517bb5aa93a6169faf6ff4e17b9d4bb384ba
Seongbin Kim <seongbin9786@gmail.com>
2018-12-26 12:44:32 +0900

    Loading Page 컴포넌트 뷰 구현

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,59 +1,124 @@
 import React from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
-import { Fade } from '@material-ui/core';
-import logoTemp from 'Assets/svg/logo-temp.svg';
-import StartDisplay from './StartDisplay';
+import {
+    Fade, Grid, Typography, LinearProgress,
+} from '@material-ui/core';
 
-const height = '-5rem';
-const styles = theme => ({
+const styles = () => ({
     root: {
+        marginTop: 140,
         height: `calc(100vh - 136px - ${/mobile/i.test(navigator.userAgent) ? 75 : 0}px)`,
         width: '100%',
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
+        '@media screen and (max-width: 360px) and (orientation: portrait)': {
+        },
+    },
+    logo: {
+        top: '-5rem',
+        position: 'relative',
+    },
+});
+const OuterGrid = withStyles({
+    container: {
+        position: 'absolute',
+        width: '100%',
+        bottom: '100px',
+        display: 'flex',
+        flexFlow: 'column',
+        padding: '0 2rem',
         '@media screen and (max-width: 360px) and  (orientation: portrait)': {
             width: '360px',
         },
     },
-    logo: {
+})(Grid);
 
+const InnerGrid = withStyles({
+    item: {
+        height: '72px',
+        display: 'flex',
+        flexFlow: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontFamily: 'Roboto',
+        fontSize: '16px',
+        fontWeight: 'normal',
+        fontStyle: 'normal',
+        fontStretch: 'normal',
+        lineHeight: '1.5',
+        letterSpacing: '0.2px',
+        textAlign: 'center',
+        color: 'rgba(0, 0, 0, 0.6)',
     },
-    '@keyframes moveUp': {
-        from: {
-            top: '0px',
-        },
-        to: {
-            top: height,
-        },
+})(Grid);
+
+const LoadingTypo = withStyles({
+    root: {
+        width: '200px',
+        height: '24px',
+        fontFamily: 'Roboto',
+        fontSize: '16px',
+        fontWeight: 'normal',
+        fontStyle: 'normal',
+        fontStretch: 'normal',
+        lineHeight: '1.5',
+        letterSpacing: '0.2px',
+        textAlign: 'center',
+        color: 'rgba(0, 0, 0, 0.38)',
     },
-    movelogo: {
-        top: height,
-        position: 'relative',
-        animationName: 'moveUp',
-        animationDuration: '0.5s',
+})(Typography);
+
+const LoadingBar = withStyles({
+    root: {
+        height: 4,
+        backgroundColor: '#BFBFBF',
     },
-});
+    bar: {
+        backgroundColor: '#3eafa2',
+    },
+})(LinearProgress);
 
 class Loading extends React.Component {
     state = {
-        loaded: false,
-    }
+        completed: 0,
+    };
 
     componentDidMount() {
-        setTimeout(() => this.setState({ loaded: true }), 1500);
+        this.timer = setInterval(this.progress, 500);
     }
+
+    componentWillUnmount() {
+        clearInterval(this.timer);
+    }
+
+    progress = () => {
+        const { completed } = this.state;
+        if (completed === 100) {
+            this.setState({ completed: 0 });
+        } else {
+            const diff = Math.random() * 10;
+            this.setState({ completed: Math.min(completed + diff, 100) });
+        }
+    };
 
     render() {
         const { classes, handleClick } = this.props;
-        const { loaded } = this.state;
+        const { completed } = this.state;
 
         return (
             <div className={classes.root}>
-                <Fade in timeout={{ enter: 1000 }}>
-                    <img src={`${logoTemp}`} alt="" className={classes[loaded ? 'movelogo' : 'logo']} />
+                <Fade in timeout={1000}>
+                    <img src="/loading_logo.svg" alt="Loading Logo" className={classes.logo} />
                 </Fade>
-                { loaded && <StartDisplay handleClick={handleClick} />}
+                <Fade in timeout={1500}>
+                    <OuterGrid container>
+                        <InnerGrid item>
+                            <LoadingTypo>Loading...</LoadingTypo>
+                        </InnerGrid>
+                        <LoadingBar variant="determinate" value={completed} />
+                    </OuterGrid>
+                </Fade>
             </div>
         );
     }
