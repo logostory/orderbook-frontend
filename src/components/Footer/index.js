b5798d84c4c1f1b4b3a5fbaedb18ec1bf89afc28
Seongbin Kim <seongbin9786@gmail.com>
2019-01-04 13:20:33 +0900

    Footer 디자인 업데이트

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,51 +1,18 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styles from './styles.module.css';
 
-import arrowRightIcon from '../../images/baseline-chevron_right-24px.svg';
-
-const Cart = ({ totalPrice, totalCount }) => (
-  <div className={styles.bill}>
-    <div className={styles.row}>
-      <div className={styles.billTitle}>주문메뉴</div>
-      <div className={styles.billContent}>{totalCount}</div>
-    </div>
-    <div className={styles.row}>
-      <div className={styles.billTitle}>총 가격</div>
-      <div className={styles.billContent}>{totalPrice}</div>
-    </div>
-  </div>
+const Footer = ({
+    totalPrice = 0,
+    text = 'VIEW MY ORDER',
+    onClick = f => f,
+}) => (
+    <footer className={styles.footer}>
+        <div className={styles.textContainer}>
+            <span>TOTAL</span>
+            <span className={styles.moneyText}>{`${totalPrice} WON`}</span>
+        </div>
+        <button className={styles.button} type="button" onClick={onClick}>{text.toUpperCase()}</button>
+    </footer>
 );
-
-const Footer = ({ text }) => (
-  <footer className={styles.footer}>
-    <Cart />
-    <div className={styles.next}>
-      <span>
-        {text}
-        <img src={arrowRightIcon} alt="다음" />
-      </span>
-    </div>
-  </footer>
-);
-
-
-Cart.propTypes = {
-  totalCount: PropTypes.number,
-  totalPrice: PropTypes.number,
-};
-
-Cart.defaultProps = {
-  totalCount: 0,
-  totalPrice: 0,
-};
-
-Footer.propTypes = {
-  text: PropTypes.string,
-};
-
-Footer.defaultProps = {
-  text: '주문하기',
-};
 
 export default Footer;
