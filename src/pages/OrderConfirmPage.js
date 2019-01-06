f30b01065eb64198df540acb028f194a7415652b
circleGiven <circlegiven@gmail.com>
2019-01-06 13:20:24 +0900

    #28 change order confirm page location

--- src/pages/OrderConfirmPage.js
+++ src/pages/OrderConfirmPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import OrderConfirm from "../components/Order/OrderConfirm";
 import {withRouter} from "react-router-dom";
+import queryString from 'query-string';
 
 class OrderConfirmPage extends React.Component {
 
@@ -15,8 +16,11 @@ class OrderConfirmPage extends React.Component {
 
     render() {
         const { handleClickConfirm, handleClickAbout } = this;
+        const { location: { search }} = this.props;
+        const { number } = queryString.parse(search);
+
         return (
-            <OrderConfirm handleClickConfirm={handleClickConfirm} handleClickAbout={handleClickAbout} orderNumber={'A21'}/>
+            <OrderConfirm handleClickConfirm={handleClickConfirm} handleClickAbout={handleClickAbout} orderNumber={number}/>
         );
     }
 }
